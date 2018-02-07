3f1e49407363b60273ece7f947d47b0c1069aed3
sbritorodr <36245871+sbritorodr@users.noreply.github.com>
2018-02-07 21:35:06 +0000

    Update light quadratic equation solver.py

--- light quadratic equation solver.py
+++ light quadratic equation solver.py
@@ -1,18 +1,16 @@
 #calculadora ecuaciones segundo grado simple
-valor2= int(input("valor x^2 " ))
-valor1= int(input("valor x^1 " ))
-valor0= int(input("valor x " ))
-
-
+valor2= int(input("number x^2 " ))
+valor1= int(input("number x^1 " ))
+valor0= int(input("number x " ))
 
 raiz1=((valor1**2)-4*valor2*valor0)**(1/2)
 resultado1=(-valor1+raiz1)/(2*valor2)
 
-print("resultado 1: ",resultado1)
+print("result 1: ",resultado1)
 
 raiz2=((valor1**2)-4*valor2*valor0)**(1/2)
 resultado2=(-valor1-raiz2)/(2*valor2)
 
-print("resultado 2: ",resultado2)
+print("result 2: ",resultado2)
 
-input("pulsa enter para salir")
+input("press enter to exit")
